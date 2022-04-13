6f7f81a0ad582cda6e03c211f82c6c67b6f04ada
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-04-14 01:48:37 +0200

    sdm660-common: Move to common IFAAService
    Change-Id: I71bfdacc4626d75d56aa12ec384f0f116d6d6ff2

--- sdm660.mk
+++ sdm660.mk
@@ -108,12 +108,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sdm660
 
-PRODUCT_PACKAGES += \
-    org.ifaa.android.manager
-
-PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
-
 # Bluetooth
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor
@@ -232,6 +226,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/idc/uinput-fpc.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-fpc.idc \
     $(LOCAL_PATH)/idc/uinput-goodix.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-goodix.idc
+
+# IFAA manager
+PRODUCT_PACKAGES += \
+    IFAAService
 
 # Init
 PRODUCT_PACKAGES += \
