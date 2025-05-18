20b57d94234878539a312df9f69b149775fa9890
m01001101-01010110 <15172121+m01001101-01010110@users.noreply.github.com>
2025-05-18 13:18:37 +0200

    sdm660-common: Drop vestigial FM board-flags
    * No longer read anywhere.
    
    Change-Id: Iaf5d8cda03e055c4331e3a20e33c8058037b09b2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -78,12 +78,6 @@ TARGET_USES_ION := true
 # DT2W
 TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
 
-# FM
-ifeq ($(BOARD_HAVE_QCOM_FM),true)
-AUDIO_FEATURE_ENABLED_FM_POWER_OPT := true
-BOARD_HAS_QCA_FM_SOC := cherokee
-endif
-
 # GPS
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
 LOC_HIDL_VERSION := 3.0
