557a2f0e5d8f5a24e62a2dd7126b447708255b69
MV <15172121+m01001101-01010110@users.noreply.github.com>
2025-07-13 00:17:54 +0200

    fixup! sdm660-common: doze: Migrate to CompoundButton.OnCheckedChangeListener

--- doze/src/org/lineageos/settings/doze/DozeSettingsFragment.java
+++ doze/src/org/lineageos/settings/doze/DozeSettingsFragment.java
@@ -26,6 +26,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 
 import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
