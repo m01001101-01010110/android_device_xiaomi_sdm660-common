310094001cef0edd4493454ca5a0e342350e644c
MV <15172121+m01001101-01010110@users.noreply.github.com>
2025-07-12 08:57:35 +0200

    sdm660-common: Move to new RFS install_symlink targets

--- sdm660.mk
+++ sdm660.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Add common definitions for Qualcomm
+$(call inherit-product, hardware/qcom-caf/common/common.mk)
+
 # Disable APEX compression
 # Keep this after including updatable_apex.mk
 PRODUCT_COMPRESSED_APEX := false
