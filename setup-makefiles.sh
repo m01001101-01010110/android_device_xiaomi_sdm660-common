c2672885b5ef5f18d5b818dd2860bc8e419e8765
MV <15172121+m01001101-01010110@users.noreply.github.com>
2025-08-18 21:48:14 +0200

    extract-utils: Allow overriding VENDOR with VENDOR_COMMON

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -22,7 +22,7 @@ fi
 source "${HELPER}"
 
 # Initialize the helper for common
-setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
+setup_vendor "${DEVICE_COMMON}" "${VENDOR_COMMON:-$VENDOR}" "${ANDROID_ROOT}" true
 
 # Warning headers and guards
 write_headers "jasmine_sprout jason lavender platina twolip wayne whyred"
@@ -41,7 +41,7 @@ echo "endif" >> "${PRODUCTMK}"
 # Finish
 write_footers
 
-if [ -s "${MY_DIR}/../$DEVICE_SPECIFIED_COMMON/proprietary-files.txt" ]; then
+if [ -s "${MY_DIR}/../../${VENDOR}/$DEVICE_SPECIFIED_COMMON/proprietary-files.txt" ]; then
     DEVICE_COMMON="${DEVICE_SPECIFIED_COMMON}"
 
     # Reinitialize the helper for device specified common
@@ -51,7 +51,7 @@ if [ -s "${MY_DIR}/../$DEVICE_SPECIFIED_COMMON/proprietary-files.txt" ]; then
     write_headers "${DEVICE_SPECIFIED_COMMON_DEVICE}"
 
     # The standard device specified common blobs
-    write_makefiles "${MY_DIR}/../${DEVICE_SPECIFIED_COMMON}/proprietary-files.txt"
+    write_makefiles "${MY_DIR}/../../${VENDOR}/${DEVICE_SPECIFIED_COMMON}/proprietary-files.txt"
 
     # Finish
     write_footers
@@ -59,7 +59,7 @@ if [ -s "${MY_DIR}/../$DEVICE_SPECIFIED_COMMON/proprietary-files.txt" ]; then
     DEVICE_COMMON="sdm660-common"
 fi
 
-if [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
+if [ -s "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-files.txt" ]; then
     # Reinitialize the helper for device
     setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false
 
@@ -67,7 +67,7 @@ if [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
     write_headers
 
     # The standard device blobs
-    write_makefiles "${MY_DIR}/../${DEVICE}/proprietary-files.txt"
+    write_makefiles "${MY_DIR}/../../${VENDOR}/${DEVICE}/proprietary-files.txt"
 
     # Finish
     write_footers
